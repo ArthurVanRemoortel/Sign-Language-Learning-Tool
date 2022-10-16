0b9839ca29db12a484e7f1fae50f7ad31bf0b0d7
ArthurVanRemoortel <arthurvanremoortel@gmail.com>
2022-10-16 15:03:19 +0200

    feat: Added AI notebooks and AI dependencies to projects.

---
+++ sl_ai/__init__.py
@@ -0,0 +1,16 @@
+import ffmpeg
+
+
+def ffmpeg_resize(vid_path, output_path, width, height):
+    """
+    use ffmpeg to resize the input person_video to the width given, keeping aspect ratio
+    """
+    input_vid = ffmpeg.input(vid_path, an=None)
+    vid = (
+        input_vid
+        .filter('scale', width, height)
+        .output(output_path, loglevel="quiet")
+        .overwrite_output()
+        .run()
+    )
+    return output_path
