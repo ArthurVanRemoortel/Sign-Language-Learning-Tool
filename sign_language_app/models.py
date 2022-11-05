3dc87a1b00956834c5449149ef947e404a1204eb
ArthurVanRemoortel <arthurvanremoortel@gmail.com>
2022-11-05 14:17:28 +0100

    feat: Added models for courses, units and gestures.

--- sign_language_app/models.py
+++ sign_language_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Gesture(models.Model):
+    word = models.CharField(max_length=100)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=200)
+
+
+class Unit(models.Model):
+    class Difficulty(models.TextChoices):
+        BEGINNER = '1', _('Beginner')
+        INTERMEDIATE = '2', _('Intermediate')
+        ADVANCED = '3', _('Advanced')
+
+    models.CharField(max_length=100)
+    gestures = models.ManyToManyField(Gesture)
+
+    difficulty = models.CharField(
+        max_length=2,
+        choices=Difficulty.choices,
+        default=Difficulty.BEGINNER,
+    )
+
+    def get_difficulty(self) -> Difficulty:
+        # Get value from choices enum
+        return self.Difficulty[self.difficulty]
+
