8773579384ddd2d067f36caa9e774b2f2a6b756c
ArthurVanRemoortel <arthurvanremoortel@gmail.com>
2022-11-12 15:55:48 +0100

    fix: Post landmarks to API in correct format.

--- sign_language_app/views/api.py
+++ sign_language_app/views/api.py
@@ -1,16 +1,18 @@
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-
+from pprint import pprint
 
 @api_view(['POST'])
 @authentication_classes([SessionAuthentication, BasicAuthentication])
 @permission_classes([IsAuthenticated])
 def test_auth(request):
-    data = JSONParser().parse(request)
-    print(data)
+    data = request.data
+    pprint(data['left_landmarks'])
+    pprint(data['right_landmarks'])
     return JsonResponse({'status': 'OK'}, status=status.HTTP_201_CREATED)
