879faee0c7c076309c6980a5882c5e47e5758cba
ArthurVanRemoortel <arthurvanremoortel@gmail.com>
2022-11-18 17:06:50 +0100

    feat: Courses page now renders real courses and units and not placeholders.

--- sign_language_app/views/views.py
+++ sign_language_app/views/views.py
@@ -1,7 +1,10 @@
+from pprint import pprint
+
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from sign_language_app.models import *
 
 def index(request):
     context = {}
@@ -9,7 +12,14 @@ def index(request):
 
 
 def courses_overview(request):
-    context = {}
+    courses = Course.objects.all()
+    user = request.user
+    context = {
+        "courses": courses,
+        "completed_units": [attempt.unit for attempt in UnitAttempt.objects.filter(user=user)],
+        "next_units": {course.id: course.get_next_unit(user) for course in courses}
+    }
+    pprint(context)
     return render(request, "sign_language_app/courses/overview.html", context)
 
 
