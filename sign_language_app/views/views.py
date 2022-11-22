23d6f04c8743eb6d1a4528b2b8b6d57078d39198
ArthurVanRemoortel <arthurvanremoortel@gmail.com>
2022-11-22 16:22:12 +0100

    fix: Anonymous users can still browse the website but practice.

--- sign_language_app/views/views.py
+++ sign_language_app/views/views.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.db.models import Q
+from django.db.models import Q, F
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.paginator import Paginator
@@ -10,9 +10,17 @@ from sign_language_app.forms import CoursesForm
 from sign_language_app.models import *
 
 
+def get_user(request):
+    return request.user if not request.user.is_anonymous else None
+
+
 def index(request):
     context = {}
     return render(request, "sign_language_app/index.html", context)
+
+
+class JoinedField:
+    pass
 
 
 def courses_overview(request, page=1):
@@ -25,9 +33,16 @@ def courses_overview(request, page=1):
             search_input_text = search_form.cleaned_data.get("search_input", None)
             if search_input_text:
                 courses = courses.filter(Q(name__icontains=search_input_text) | Q(description__icontains=search_input_text) | Q(units__name__icontains=search_input_text)).distinct()
-    user = request.user
+
+    completed_units = []
+    next_units = {}
+    user = get_user(request)
+    if user:
+        completed_units = [attempt.unit for attempt in UnitAttempt.objects.filter(user=user)]
+        next_units = {course.id: course.get_next_unit(user) for course in courses}
 
     courses = courses.union(*[courses for _ in range(100)], all=True)  # Hacky way to make the results longer to test pagination.
+
 
     paginator = Paginator(courses, 15)
     page_number = request.GET.get('page', 1)
@@ -38,12 +53,13 @@ def courses_overview(request, page=1):
         "courses": courses,
         "search_form": search_form,
         'courses_paginator': courses_paginator,
-        "completed_units": [attempt.unit for attempt in UnitAttempt.objects.filter(user=user)],
-        "next_units": {course.id: course.get_next_unit(user) for course in courses}
+        "completed_units": completed_units,
+        "next_units": next_units
     }
     return render(request, "sign_language_app/courses_overview.html", context)
 
 
+@login_required
 def unit_view(request, unit_id):
     unit = get_object_or_404(Unit, pk=unit_id)
     context = {
