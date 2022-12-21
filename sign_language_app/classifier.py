3ce7363b69f1a75f5c9e4e316de7e237d54ac3ae
ArthurVanRemoortel <arthurvanremoortel@gmail.com>
2022-12-21 09:53:52 +0100

    fix: Does not launch if uploaded_gestures_dataset.csv is missing.

--- sign_language_app/classifier.py
+++ sign_language_app/classifier.py
@@ -4,14 +4,18 @@ from pathlib import Path
 from sl_ai.dataset import GestureDataset
 from sl_ai.gesture_classifier import GestureClassifier
 
+gestures_path = Path('sl_ai/gestures_dataset.csv')
 gesture_dataset: GestureDataset = GestureDataset()
-gesture_dataset.load_from_csv(Path('sl_ai/gestures_dataset.csv'))
+gesture_dataset.load_from_csv(gestures_path)
 
-uploaded_dataset: GestureDataset = GestureDataset()
-uploaded_dataset.load_from_csv(Path('sl_ai/uploaded_gestures_dataset.csv'))
 
-gesture_dataset.append_dataset(uploaded_dataset)
-gesture_dataset.summary()
+uploaded_gesture_path = Path('sl_ai/uploaded_gestures_dataset.csv')
+if uploaded_gesture_path.exists():
+    uploaded_dataset: GestureDataset = GestureDataset()
+    uploaded_dataset.load_from_csv(uploaded_gesture_path)
+
+    gesture_dataset.append_dataset(uploaded_dataset)
+    gesture_dataset.summary()
 
 gesture_classifier: GestureClassifier = GestureClassifier(gesture_dataset=gesture_dataset)
 
