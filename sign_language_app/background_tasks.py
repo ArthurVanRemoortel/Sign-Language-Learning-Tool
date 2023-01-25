5c8228d3524b743ca663e0c92f6c010835111972
ArthurVanRemoortel <arthurvanremoortel@gmail.com>
2023-01-25 22:44:17 +0100

    fix: uploaded gestures status was not updated correctly

--- sign_language_app/background_tasks.py
+++ sign_language_app/background_tasks.py
@@ -3,11 +3,12 @@ import random
 import threading
 import time
 from pathlib import Path
+from typing import List
 
 import schedule
 from django.db.models import Q
 
-from learning_site.settings import BACKGROUND_TRAINING_TIME
+from learning_site.settings import BACKGROUND_TRAINING_TIME, UPLOADED_GESTURES_ROOT
 from sign_language_app.classifier import Classifier
 from sign_language_app.models import Gesture
 from sl_ai.dataset import GestureDataset
@@ -15,22 +16,24 @@ from sl_ai.dataset import GestureDataset
 IS_TRAINING = False
 
 
-def retrain_thread(new_gestures):
+def retrain_thread(new_gestures: List[Gesture]):
+    if not new_gestures:
+        return
     global IS_TRAINING
     IS_TRAINING = True
     print("Starting training...")
     csv_out_path = Path('sl_ai/uploaded_gestures_dataset.csv')
     model_path = Path('sl_ai/model.h5')
     new_dataset = GestureDataset()
-
-    # TODO: Should return the data. Writing to csv should be be separate function.
+    new_dataset.scan_videos(UPLOADED_GESTURES_ROOT, handedness_data={g.word: (g.left_hand, g.right_hand) for g in new_gestures})
     new_dataset.analyze_videos(csv_out_path=csv_out_path)
     if not csv_out_path.exists():
+        # Nothing was created.
         IS_TRAINING = False
         return
     new_dataset.load_from_csv(csv_out_path)
     for gesture in new_gestures:
-        gesture.status = Gesture.Status.COMPLETE
+        gesture.status = Gesture.Status.TRAINING
         gesture.save()
 
     # TODO: Add methods to Classifier class
@@ -38,7 +41,7 @@ def retrain_thread(new_gestures):
     Classifier().gesture_classifier.train(save_path=model_path)
 
     for gesture in new_gestures:
-        gesture.status = Gesture.Status.TRAINING
+        gesture.status = Gesture.Status.COMPLETE
         gesture.save()
         new_dataset.add_django_gesture(gesture)
 
